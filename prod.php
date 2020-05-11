c13c60cf2375116d2aa870bf85cda2f6a5e0e53b
MaxiVargas78 <maxx7689@gmail.com>
2020-05-11 01:18:58 -0400

    act. detalle productos con MongoDB

--- prod.php
+++ prod.php
@@ -2,8 +2,15 @@
 include_once("header.php");
 include_once("datos.php");
 $prod = $_GET['key'];
-$nombre = $productos[$prod];
-$desc = $descripciones[$prod];
+
+require 'vendor/autoload.php';
+$uri="mongodb://localhost";
+$client=new MongoDB\Client($uri);
+
+$producto= $client->infomaxi->productos->findOne(['_id' => MongoDB\BSON\ObjectID($prod)]);
+
+$nombre = $producto['name'];
+$desc = $producto['desc'];
 ?> 
 	<h3><?php echo $nombre?></h3>
     <h><i><?php echo $desc;?></i></h>
