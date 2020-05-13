b4f7e29468e43057809236583c351057bafe178b
MaxiVargas78 <maxx7689@gmail.com>
2020-05-13 00:11:52 -0400

    imp. css 1.2

--- header.php
+++ header.php
@@ -20,8 +20,8 @@
       </nav>
     </div>
   <section class="jumbotron text-center">
+    <img src="https://www.bbva.com/wp-content/uploads/2018/06/fintech-tecnologia-innovacion-digital-blockchain-bbva_opt-1024x431.jpg" class="img-fluid" alt="Responsive image">
     <div class="container">
-      <img src="https://www.bbva.com/wp-content/uploads/2018/06/fintech-tecnologia-innovacion-digital-blockchain-bbva_opt-1024x431.jpg" class="img-fluid" alt="Responsive image">
       <h1>Bienvenido a InfoMax</h1>
       <p class="lead text-muted">Conectando Soluciones</p>
       <p class="lead text-muted">Encuentra todo lo necesario para tu Pyme, Empresa o Institucion</p>
