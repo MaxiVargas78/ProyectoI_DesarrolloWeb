c5496b2e985493fa7ab9e158ed04e5d3402fd6d2
MaxiVargas78 <maxx7689@gmail.com>
2020-05-13 01:15:20 -0400

    imp. css 1.2

--- header.php
+++ header.php
@@ -37,7 +37,7 @@
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="img/Slide1.jpg" height="400" class="d-block w-100" alt="">
+      <img src="img/Slide1.jpg" class="d-block w-100" alt="">
       <div class="carousel-caption d-none d-md-block">
         <h5></h5>
         <p></p>
