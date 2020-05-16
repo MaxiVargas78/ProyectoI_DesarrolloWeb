2335124d543b8ed41d8ba52ab3fa5ab496aa3ff8
MaxiVargas78 <maxx7689@gmail.com>
2020-05-16 18:03:48 -0400

    Arreglando index

--- index.php
+++ index.php
@@ -15,11 +15,32 @@ $cat= $_GET['key'];
 ?> 
 <div class="alert alert.primary" role="alert">
 	<h3>Elija Servicio</h3>
-	<ul class="gallery-container">
-        <div class="gallery__item"><img src="" alt="Descripcion 1" class="gallery__img"> </div>
-        <div class="gallery__item"><img src="" alt="Descripcion 2" class="gallery__img"> </div>
-		<div class="gallery__item"><img src="" alt="Descripcion 3" class="gallery__img"> </div>
- </ul>
+	<div class="card-group">
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
 	<?php
 	foreach($categorias as $key => $cat){
 	?>
