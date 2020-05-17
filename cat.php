b7dfa424fb1545faddb81aa81318096fada10161
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 19:57:00 -0400

    header & index terminados + arreglo img y css cat

--- cat.php
+++ cat.php
@@ -20,7 +20,7 @@ foreach ($collection as $entry) {
 ?> 
 <div class="card bg-dark mb-3">
 	<h5 class="card-header text-white bg-dark mb-3">Servicio Internet</h5>
-	<ul class="list-group">
+	<ul class="list-group list-group-flush">
   <?php
 	foreach($prods as $key => $value){
 	?>
