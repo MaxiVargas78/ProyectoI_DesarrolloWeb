3309046cfd814fca82ce53dc22698c76c7c918f4
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 12:57:09 -0400

    Act. index

--- index.php
+++ index.php
@@ -24,14 +24,14 @@ $cat= $_GET['key'];
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="img/internet.jpg" weight="100" height="100" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
     </div>
   </div>
   <div class="card">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="img/internet.jpg" weight="100" height="100" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
