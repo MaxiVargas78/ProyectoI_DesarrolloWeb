4ce862788ec2f01bbb01511291607f08536081d6
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 19:33:25 -0400

    header & index terminados + arreglo img y css cat

--- cat.php
+++ cat.php
@@ -24,7 +24,7 @@ foreach ($collection as $entry) {
   <?php
 	foreach($prods as $key => $value){
 	?>
-		 <li class="list-group-item bg-dark mb-3"> <a href="prod.php?key=<?php echo $key?>"><?php echo $value?></a></li>";
+		 <li class="list-group-item text-white bg-dark mb-3"> <a href="prod.php?key=<?php echo $key?>"><?php echo $value?></a></li>
 	<?php
 	}
 	?>
