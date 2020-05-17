e6137b8b77adbd373fe23cefbfe48ce992c48276
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 14:53:59 -0400

    Act. index

--- index.php
+++ index.php
@@ -17,12 +17,11 @@ $cat= $_GET['key'];
   <div class="card">
     <img src="img/internet.jpg" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      <a class="nav-link active" href="#">Active</a>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
+      <h5 class="card-title">Servicio Internet</h5>
+      <p class="card-text">Encuentra los mejores planes y servicios de Internet.</p>
+      <li class="nav-item">
+        <a class="nav-link active" href="cat.php?key=<?php echo $key?>">Ver Más</a>
+      </li>
     </div>
   </div>
   <div class="card">
