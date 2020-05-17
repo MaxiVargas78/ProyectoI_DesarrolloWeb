991ab8be823c5a2be8bc29a6912c39aaa80f9a06
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 18:22:42 -0400

    header & index terminados + arreglo img y css cat

--- cat.php
+++ cat.php
@@ -21,7 +21,7 @@ foreach ($collection as $entry) {
 <div class="card mb-3" style="max-width: 540px;">
   <div class="row no-gutters">
     <div class="col-md-4">
-      <img src="..." class="card-img" alt="...">
+      <img src="img/internet.jpg" class="card-img" alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
@@ -32,9 +32,6 @@ foreach ($collection as $entry) {
 	}
 	?>
 	</ul>
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
       </div>
     </div>
   </div>
