cf3ae45b6b2221d754baff21d65b382f7149cd83
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 22:55:13 -0400

    Finalizado

--- somos.php
+++ somos.php
@@ -2,12 +2,12 @@
 include_once("header.php");
 include_once("datos.php");
 ?>
+<h5 class="card-header bg-dark mb-3">Quienes Somos</h5>
 <div class="card mb-3" style="max-width: 540px;">
   <div class="row no-gutters">
     <div class="col-md-4">
       <img src="https://images.vexels.com/media/users/3/142890/isolated/lists/4ea2d7c4bf3cad23a4f18ee58752deb8-anillos-de-alta-tecnologia-logo.png"class="card-img" alt="...">
     </div>
-    <h5 class="card-header bg-dark mb-3">Quienes Somos</h5>
     <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">InfoMax</h5>
