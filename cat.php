ffb30b200a0b6f539af1e484bb5de1b7f0d78655
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 21:07:22 -0400

    header & index terminados + arreglo img y css cat

--- cat.php
+++ cat.php
@@ -8,7 +8,13 @@ $uri="mongodb://localhost";
 $client=new MongoDB\Client($uri);
 
 $cat= $_GET['key'];
-$producto= $client->infomaxi->productos->findOne(['_id' => new MongoDB\BSON\ObjectID($cat)]);
+$collection= $client->infomaxi->productos->find(['categoria' => $cat]);
+$prods = array();
+foreach ($collection as $entry) {
+    $prods[ $entry['_id']->__toString() ] = $entry['name'];
+
+}
+$producto= $client->infomaxi->productos->findOne(['_id' => new MongoDB\BSON\ObjectID($prod)]);
 
 $nombre = $producto['name'];
 $desc = $producto['desc'];
