e9605ddb3c33ec59e6290508184a2220b3a5262e
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 20:32:46 -0400

    header & index terminados + arreglo img y css cat

--- prod.php
+++ prod.php
@@ -13,12 +13,18 @@ $nombre = $producto['name'];
 $desc = $producto['desc'];
 $img = $producto['img'];
 ?> 
-<div class="card mb-3">
-  <img src="<?php echo $img;?>" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title"><?php echo $nombre?></h5>
-    <p class="card-text"><?php echo $desc;?></p>
-   
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="<?php echo $img;?>" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $nombre?></h5>
+        <p class="card-text"><?php echo $desc;?></p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
   </div>
 </div>
 <?php
