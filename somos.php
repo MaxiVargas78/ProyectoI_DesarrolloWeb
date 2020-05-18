0ccb751ce862ea4076d48f06227abcf1bdd3b62f
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 22:54:27 -0400

    Finalizado

--- somos.php
+++ somos.php
@@ -7,8 +7,8 @@ include_once("datos.php");
     <div class="col-md-4">
       <img src="https://images.vexels.com/media/users/3/142890/isolated/lists/4ea2d7c4bf3cad23a4f18ee58752deb8-anillos-de-alta-tecnologia-logo.png"class="card-img" alt="...">
     </div>
-    <div class="col-md-8">
     <h5 class="card-header bg-dark mb-3">Quienes Somos</h5>
+    <div class="col-md-8">
       <div class="card-body">
         <h5 class="card-title">InfoMax</h5>
         <p class="card-text">Somos una empresa la cual busca canalizar todas las necesidades de nuestros clientes en el área de informática y desarrollo. Doce años de trayectoria nos avalan como pioneros en servicios integrados al alcance de todos.</p>
