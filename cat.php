69764c9d10e2142ecd26d4ccb5bbc168f46beb6d
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 20:58:51 -0400

    header & index terminados + arreglo img y css cat

--- cat.php
+++ cat.php
@@ -1,36 +1,33 @@
 <?php
 include_once("header.php");
+include_once("datos.php");
+$prod = $_GET['key'];
 
 require 'vendor/autoload.php';
 $uri="mongodb://localhost";
 $client=new MongoDB\Client($uri);
 
-$cat= $_GET['key'];
-$collection= $client->infomaxi->productos->find(['categoria' => $cat]);
-$prods = array();
-foreach ($collection as $entry) {
-    $prods[ $entry['_id']->__toString() ] = $entry['name'];
+$producto= $client->infomaxi->productos->findOne(['_id' => new MongoDB\BSON\ObjectID($prod)]);
 
-}
-
-
-
-//print_r($prods);
-//print_r($productos[$cat]);
+$nombre = $producto['name'];
+$desc = $producto['desc'];
+$img = $producto['img'];
 ?> 
-<div class="card bg-dark mb-3">
-	<h5 class="card-header text-white bg-dark mb-3">Servicio Internet</h5>
-	<ul class="list-group bg-dark mb-3">
-  <?php
-	foreach($prods as $key => $value){
-	?>
-		 <li class="list-group-item list-group-item-success text-white bg-dark mb-3"> <a href="prod.php?key=<?php echo $key?>"><?php echo $value?></a></li>
-	<?php
-	}
-	?>
+<h5 class="card-header text-white bg-dark mb-3">Productos</h5>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="<?php echo $img;?>" weight="500" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $nombre?></h5>
+        <p class="card-text"><?php echo $desc;?></p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
   </div>
 </div>
-</ul>
 <?php
 include_once("footer.php");
 ?>
