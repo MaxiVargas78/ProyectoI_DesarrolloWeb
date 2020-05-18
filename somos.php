de3550d648fa57628e6f8fa329ea29b2bae27e90
MaxiVargas78 <maxx7689@gmail.com>
2020-05-17 22:40:01 -0400

    Finalizado

--- somos.php
+++ somos.php
@@ -4,9 +4,9 @@ include_once("datos.php");
 ?>
 
 <div class="card bg-dark mb-3">
-  <h5 class="card-header bg-dark mb-3">Quienes Somos</h5>
+  <h class="card-header bg-dark mb-3">Quienes Somos</h5>
   <div class="card-body">
-    <h5 class="card-title">InfoMax</h5>
+    <h class="card-title">InfoMax</h5>
     <p class="card-text">Somos una empresa la cual busca canalizar todas las necesidades de nuestros clientes en el área de informática y desarrollo. Doce años de trayectoria nos avalan como pioneros en servicios integrados al alcance de todos.</p>
     <a href="/index.php" class="btn btn-primary">Conoce nuestros productos</a>
   </div>
