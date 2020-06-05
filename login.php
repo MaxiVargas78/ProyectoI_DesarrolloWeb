21cd48adbf7f9b8ee48be487dce0f157d152ba37
MaxiVargas78 <maxx7689@gmail.com>
2020-06-05 16:26:22 -0400

    Page Login

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+include_once("header.php");
+?>
+<center>
+<form action="redirect.php" method="POST">
+Ingrese Nombre de Usuario:
+<input type="text" name="usuario">
+<input type="submit" value="Ingresar">
+
+</form>
+</center>
+
+<?php
+include_once("footer.php");
+?>
