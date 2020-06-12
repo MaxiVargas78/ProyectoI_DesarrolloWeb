a0d0d4bc95f843c6ebffdcf375ebf390c8644d79
MaxiVargas78 <maxx7689@gmail.com>
2020-06-12 02:39:55 -0400

    Act. Login

--- login.php
+++ login.php
@@ -1,14 +1,14 @@
 <?php
 include_once("header.php");
 ?>
-<div class="card bg-dark" style="width: 36rem;"  >
+<div class="card bg-dark text-white" style="width: 36rem;"  >
   <div class="card-header">
     Inicia Sesión
   </div>
 <form action="redirect.php" method="POST">
   <div class="form-group">
     <label for="exampleInputEmail1">Nombre de Usuario</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input style="width: 25rem;" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
     <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo electrónico con nadie más.</small>
   </div>
   </div>
