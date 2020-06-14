845142598e2449787ca4a39b1f336463f2c24d34
MaxiVargas78 <maxx7689@gmail.com>
2020-06-14 02:42:28 -0400

    Registro + Login

--- login.php
+++ login.php
@@ -8,14 +8,18 @@ INICIAR SESIÓN
 <div class="card-deck">
 <div class="card bg-dark text-white" style="width: 36rem;"  >
   <div class="card-header">
-    Clientes
+    LOGIN
   </div>
 <form action="redirect.php" method="POST">
 <center>
   <div class="form-group m-5 pb-5">
     <label for="exampleInputEmail1">Nombre de Usuario</label>
     <input style="width: 25rem;" type="text" name="usuario" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos sus datos con nadie más.</small>
+    <div class="form-group">
+    <label  for="exampleInputPassword1">Contraseña</label>
+    <input style="width: 25rem;" type="password" name="password" class="form-control" id="exampleInputPassword1">
+  </div>
+    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos sus datos con nadie.</small>
     <button type="submit"  value="Ingresar" class="btn btn-primary pull-xs-right">Ingresar</button>
 
     </form>
@@ -27,7 +31,7 @@ INICIAR SESIÓN
 <div class="card bg-dark text-white mb-3" style="width: 36rem;"  >
 
   <div class="card-header">
-    Administrador
+    REGISTRARSE
   </div>
   <form action="redirect1.php" method="POST">
 <center>
@@ -37,6 +41,10 @@ INICIAR SESIÓN
   </div>
   <div class="form-group">
     <label  for="exampleInputPassword1">Contraseña</label>
+    <input style="width: 25rem;" type="password" name="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="form-group">
+    <label  for="exampleInputPassword1"> Confirmar Contraseña</label>
     <input style="width: 25rem;" type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
   <button type="submit" value="ingresar" class="btn btn-primary">Ingresar</button>
