9293f24ddc9539f276d721c11c97ee5b2508de2c
MaxiVargas78 <maxx7689@gmail.com>
2020-06-13 22:37:54 -0400

    correccion boton eliminar

--- carritocompra.php
+++ carritocompra.php
@@ -40,7 +40,7 @@ $producto= $client->infomaxi->productos->findOne(['_id' => new MongoDB\BSON\Obje
     <td>$<?php echo $producto['precio']*$cantidad;?></td>
 
     <td>
-    <a  href="carrito.php?remover=<?php echo $prod; ?>">
+    <a  href="carritocompra.php?remover=<?php echo $prod; ?>">
     <svg class="bi bi-x" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
   <path fill-rule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
   <path fill-rule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
