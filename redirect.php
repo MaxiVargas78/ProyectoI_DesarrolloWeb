c43256dd4b4ba3d53e9d3574a53624478a670f05
MaxiVargas78 <maxx7689@gmail.com>
2020-06-14 03:16:54 -0400

    Registro + Login

--- redirect.php
+++ redirect.php
@@ -1,6 +1,28 @@
 <?php
+require 'vendor/autoload.php';
+$uri="mongodb://localhost";
+$client=new MongoDB\Client($uri);
+
 session_start();
 $_SESSION['usuario'] = $_POST['usuario'];
 header("Location: /");
 
+$usuario = $_POST["usuario"];
+$password = $_POST["password"];
+
+$user = [
+    '_id' => new MongoDB\BSON\ObjectId,
+    'usuario' => $usuario,
+    'password' => $password
+];
+try{
+    $client->infomaxi->insert($user);
+    $result = $manager->executeClientWrite($infomaxi, $client);
+    header("Location: /");
+}
+catch(MongoDB\Client\Exception\Exception $e){
+    die("Error encontrado".$e);
+
+
+}
 ?>
