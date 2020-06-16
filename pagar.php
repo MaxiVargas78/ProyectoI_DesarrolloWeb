63d5a11d266d99657ec69251a6baadb2d7580f5f
MaxiVargas78 <maxx7689@gmail.com>
2020-06-16 04:46:45 -0400

    act. pagar

--- pagar.php
+++ pagar.php
@@ -12,7 +12,20 @@ $client=new MongoDB\Client($uri);
 $ordenes = $client->infomaxi->ordenes;
 $ordenes->insertOne(array('total' => $total, 'productos'=> $_SESSION['carrito'], 'usuario' => $_SESSION['usuario']));
 unset($_SESSION['carrito']);
-echo "<h2> Muchas gracias por su compra, nos contactaremos en breve con usted para la entrega</h2>";
+?>
+<div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
+  <div class="toast-header">
+    <img src="..." class="rounded mr-2" alt="...">
+    <strong class="mr-auto">InfoMax</strong>
+    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="toast-body">
+  Muchas gracias por su compra, nos contactaremos en breve con usted para la entrega!
+  </div>
+</div>";
 
+<?php
 include_once("footer.php");
 ?>
