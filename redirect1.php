70006c7da74128759cb3219688c00b24cffe1835
MaxiVargas78 <maxx7689@gmail.com>
2020-06-16 04:13:21 -0400

    login

--- redirect1.php
+++ redirect1.php
@@ -19,7 +19,7 @@ $client=new MongoDB\Client($uri);
     
       // Select Collection
     $collection = $client->infomaxi->usuarios;
-    $result = $collection->findOne(array('usuario' => $usuario,'password' =>md5($password)));
+    $result = $collection->findOne(array('usuario' => $_SESSION['usuario'],'password' =>md5($password)));
     if($result){
      echo "You are successully loggedIn";
        }
