e3bb8592b6030edc4b61b5c7b8c41dc436fc9de2
MaxiVargas78 <maxx7689@gmail.com>
2020-06-16 01:21:44 -0400

    fix register

---
+++ ordenes.php
@@ -0,0 +1,36 @@
+<?php
+include_once("header.php");
+include_once("datos.php");
+$prod = $_GET['key'];
+
+require 'vendor/autoload.php';
+$uri="mongodb://localhost";
+$client=new MongoDB\Client($uri);
+
+$ordenes= $client->infomaxi->ordenes->findOne(['_id' => new MongoDB\BSON\ObjectID($prod)]);
+
+$usuario = $ordenes['usuario'];
+$productos = $ordenes['productos'];
+$total = $ordenes['total'];
+?> 
+<h5 class="card-header text-white bg-dark mb-3">Órdenes</h5>
+<div class="card mb-3" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="<?php echo $img;?>" weight="500" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $usuario?></h5>
+        <p class="card-text"><?php echo $ordenes;?></p>
+        <h6 class="card-text"><?php echo $total;?></h6>
+        <h6 class="card-text"> $ <?php echo $precio;?></h6>
+        </form>
+        </small></p>
+      </div>
+    </div>
+  </div>
+</div>
+<?php
+include_once("footer.php");
+?>
