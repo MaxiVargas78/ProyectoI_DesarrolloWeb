b3c667690ce543e0d6c1b1cb136546cb9be80a93
MaxiVargas78 <maxx7689@gmail.com>
2020-06-16 04:50:28 -0400

    act. pagar

--- pagar.php
+++ pagar.php
@@ -14,18 +14,11 @@ $ordenes->insertOne(array('total' => $total, 'productos'=> $_SESSION['carrito'],
 unset($_SESSION['carrito']);
 echo"<h";
 ?>
-<div role="alert" aria-live="assertive" aria-atomic="true" class="toast" data-autohide="false">
-  <div class="toast-header">
-    <img src="..." class="rounded mr-2" alt="...">
-    <strong class="mr-auto">InfoMax</strong>
-    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-  </div>
-  <div class="toast-body">
+<div class="card">
+  <div class="card-body">
   Muchas gracias por su compra, nos contactaremos en breve con usted para la entrega!
   </div>
-</div>";
+</div>
 
 <?php
 echo"</div>";
