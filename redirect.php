19c6139dfcb7c65814125d6623d2df4a532be51e
MaxiVargas78 <maxx7689@gmail.com>
2020-06-15 23:19:11 -0400

    Nueva maquina virtual

--- redirect.php
+++ redirect.php
@@ -6,20 +6,10 @@ $client=new MongoDB\Client($uri);
 session_start();
 $_SESSION['usuario'] = $_POST['usuario'];
 $_SESSION['password'] = $_POST['password'];
-print_r($_SESSION['usuario']);
+//print_r($_SESSION['usuario']);
 
 //header("Location: /");
 $usuarios = $client->infomaxi->usuarios;
-$usuarios->insertOne(array('usuario' => $usuario, 'password'=> $password));
-print_r($usuarios);
-
-try{    
-    $usuarios= $client->infomaxi->usuarios->insert($user);
-    $result = $manager->executeClient($infomaxi, $client);
-    header("Location: ../index.php");
-}
-catch(MongoDB\Client\Exception\Exception $e){
- die("Error encontrado".$e);}
-
-
-?>
+$usuarios->insertOne(array('usuario' => $_SESSION['usuario'], 'password'=> $_SESSION['password']));
+//print_r($usuarios);
+header("Location: ../index.php");
