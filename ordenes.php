22d28584b6a9b8ce95215677b136e4c1c60dba9e
MaxiVargas78 <maxx7689@gmail.com>
2020-06-24 12:40:25 -0400

    acr

--- ordenes.php
+++ ordenes.php
@@ -13,13 +13,13 @@ $client=new MongoDB\Client($uri);
 $ordenes= $client->infomaxi->ordenes->find();
 $orden2= $ordenes->toArray(); // la consulta la realiza pero no logro hacer que se visualice
 foreach($orden2 as $order ){
-    echo json_encode($order);
     echo "<tr>";
 
 
 ?>
 
     <td><?php echo $order['usuario'];?></td>
+    <td><?php echo  json_encode($order['productos']);?></td>
     <td>$<?php echo $order['total'];?></td>
   
 
