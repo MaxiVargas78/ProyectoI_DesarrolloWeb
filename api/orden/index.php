c988866f95dc55cbb637b31ac99ce82b9b56f962
MaxiVargas78 <maxx7689@gmail.com>
2020-06-30 23:23:21 -0400

    Nueva API

--- api/orden/index.php
+++ api/orden/index.php
@@ -1,4 +1,7 @@
 <?php
+require 'vendor/autoload.php';
+$uri="mongodb://localhost";
+$client=new MongoDB\Client($uri);
 
 $ordenes= $client->infomaxi->ordenes->find();
 $orden2= $ordenes->toArray(); 
