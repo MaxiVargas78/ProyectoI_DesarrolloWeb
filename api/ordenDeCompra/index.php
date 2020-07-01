f2646af16b88f5c3367266e8b1ea799ed4c5e090
MaxiVargas78 <maxx7689@gmail.com>
2020-07-01 00:36:48 -0400

    Nueva API

--- api/ordenDeCompra/index.php
+++ api/ordenDeCompra/index.php
@@ -7,7 +7,7 @@ $client=new MongoDB\Client($uri);
 $collect= $client->infomaxi->ordenes->find();
 $orden2 = array();
 foreach ($collect as $entry) {
-        $orden2[$entry['_id']->__toString()] = $entry['name'];
+        $orden2[$entry['_id']->__toString()] = $entry['usuario'];
 }
 echo json_encode($orden2);
 
