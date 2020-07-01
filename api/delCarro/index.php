1b8fc3414dcb6dc87ef908a8b93599eacf5c0a3f
MaxiVargas78 <maxx7689@gmail.com>
2020-07-01 00:14:27 -0400

    Nueva API

--- api/delCarro/index.php
+++ api/delCarro/index.php
@@ -1,12 +1,18 @@
 <?php//elimina un elemento del carrito
+session_start();
+$producto = $_GET['prod'];
 
-include_once("header.php");
 require 'vendor/autoload.php';
 $uri="mongodb://localhost";
 $client=new MongoDB\Client($uri);
 
-if(isset($_GET['remover'])){
-    $r = $_GET['remover'];
-    unset($_SESSION['carrito'][$r]);
+if(isset($_GET['prod'])){
+	unset($_SESSION['carrito'][$producto]);
+	echo json_encode(true);
+}
+else{
+
+
+echo json_encode(false);
 }
 ?>
