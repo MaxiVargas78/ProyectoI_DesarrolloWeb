4e9a17477e6ba0670103d7cf57530aff5c74802e
MaxiVargas78 <maxx7689@gmail.com>
2020-07-01 00:32:10 -0400

    Nueva API

--- api/compraCarro/index.php
+++ api/compraCarro/index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$total = $_POST['total'];
+$total = $_GET['total'];
 require '../../vendor/autoload.php';
 $uri="mongodb://localhost";
 $client=new MongoDB\Client($uri);
@@ -8,5 +8,7 @@ $client=new MongoDB\Client($uri);
 
 $ordenes = $client->infomaxi->ordenes;
 $arr = $ordenes->insertOne(array('total' => $total, 'productos'=> $_SESSION['carrito'], 'usuario' => $_SESSION['usuario']));
+
+
 echo json_encode($arr);
 ?>
---
+++ api/compraCarro/ordenDeCompra/index.php
@@ -0,0 +1,15 @@
+<?php
+require '../../vendor/autoload.php';
+$uri="mongodb://localhost";
+$client=new MongoDB\Client($uri);
+
+
+$collect= $client->infomaxi->ordenes->find();
+$orden2 = array();
+foreach ($collect as $entry) {
+        $orden2[$entry['_id']->__toString()] = $entry['orden'];
+}
+echo json_encode($orden2);
+
+
+?>
