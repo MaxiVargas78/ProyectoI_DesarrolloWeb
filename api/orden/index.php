4a54bf20c49d5045a6c106ac42218199d9269ee3
MaxiVargas78 <maxx7689@gmail.com>
2020-06-30 23:37:57 -0400

    Nueva API

--- api/orden/index.php
+++ api/orden/index.php
@@ -4,6 +4,9 @@ $uri="mongodb://localhost";
 $client=new MongoDB\Client($uri);
 
 $ordenes= $client->infomaxi->ordenes->find();
-$orden2= $ordenes->toArray(); 
+$orden2= array();
+foreach( $ordenes as $entry){
+    $orden2[$entry['_id']->__toString()] = $entry['ordenes'];
+}
 echo json_encode($orden2);
 ?>
